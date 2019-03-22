4c5d90ab1a6f4e67a5bf1863529862015aac1322
albact7 <alba164@gmail.com>
2019-03-22 14:03:14 +0100

    User and token verification added

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 // Requires: importacion de librerias
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Inicializar variables
 var app = express();
+
+
+// Body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
 
 // COnexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res)=>{
@@ -15,13 +29,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res)=>
 
 
 // Rutas
-// request, response, next
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok : true,
-        mensaje: 'Petición realizada correctamente'
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones
