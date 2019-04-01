27620e97266027eba13d46dfa73f6f66ee81283f
albact7 <alba164@gmail.com>
2019-04-01 11:40:20 +0200

    Function findById added in medico service

--- routes/medico.js
+++ routes/medico.js
@@ -40,6 +40,35 @@ app.get('/', (req, res, next) => {
     
 });
 
+//Obtener medico
+app.get('/:id', (req, res)=>{
+    var id = req.params.id;
+
+    Medico.findById(id, (err, medico)=>{
+       
+        if(err) {
+            return res.status(500).json({
+                ok : false,
+                mensaje: 'Error buscando médico',
+                errors: err
+            });
+        }
+
+        if(!medico){
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'El médico con el id '+id+" no existe",
+                errors: {message: 'No existe un médico con ese id'}
+            });
+        }
+       
+        res.status(200).json({
+            ok : true,
+            medico: medico
+        });
+        
+    });
+});
 
 //Verificar token
 
